136e8ec98c92d0d1dc0dce16d8a535df1d8ff565
Snow <nickeysnowing@gmail.com>
2019-04-11 15:01:21 -0400

    upgrade deps

--- src/Containers/Hollow/index.tsx
+++ src/Containers/Hollow/index.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react'
 import { useQuery } from 'react-apollo-hooks'
 
+import { GetHollows_hollows } from '../../../types/client/gql/GetHollows'
 import Carousel from '../../Components/Carousel'
 import Post from '../../Components/Post'
 import { GET_HOLLOWS } from './queries'
@@ -13,7 +14,11 @@ const Hollow = () => {
   if (loading) {
     return <h1>Loading</h1>
   }
-  const [carouselList, updateCarouselList] = useState(data.hollows)
+  return <HollowWrapper hollows={data.hollows} />
+}
+
+const HollowWrapper = ({ hollows }: { hollows: GetHollows_hollows[] }) => {
+  const [carouselList, updateCarouselList] = useState(hollows)
   return (
     <Fragment>
       <Carousel carouselList={carouselList} />
